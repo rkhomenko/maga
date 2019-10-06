0868c90d84b61ad671f4d0a2c0cb6720876d3bc3
Roman Khomenko <romankhomenko1995@gmail.com>
2019-10-06 13:58:45 +0300

    Добавлены классы для тестирования производительности

--- sem01/java/maga/src/main/java/org/khomenko/maga/concurrency/site/SyncWrapperSite.java
+++ sem01/java/maga/src/main/java/org/khomenko/maga/concurrency/site/SyncWrapperSite.java
@@ -1,4 +1,28 @@
 package org.khomenko.maga.concurrency.site;
 
-public class SyncWrapperSite {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SyncWrapperSite extends Site{
+    private final Map<String, Integer> requestCounter;
+
+    public SyncWrapperSite() {
+        requestCounter = Collections.synchronizedMap(new HashMap<>());
+        getPages().forEach(page -> requestCounter.put(page, 0));
+    }
+
+    @Override
+    protected void onGetCounters() {
+        setResults(requestCounter.values());
+    }
+
+    @Override
+    public Integer getPage(String page) {
+        if (!requestCounter.containsKey(page)) {
+            throw new UnsupportedOperationException();
+        }
+        return requestCounter.merge(page, 1, Integer::sum);
+    }
 }
