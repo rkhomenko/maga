c315e7a9f457df0fc74a0e5679935077ae818119
Roman Khomenko <romankhomenko1995@gmail.com>
2019-10-30 20:36:03 +0300

    Переименован класс App

--- sem01/scala/maga/src/main/java/org/khomenko/maga/JavaHelloWorld.java
+++ sem01/scala/maga/src/main/java/org/khomenko/maga/JavaHelloWorld.java
@@ -3,12 +3,12 @@
  */
 package org.khomenko.maga;
 
-public class App {
+public class JavaHelloWorld {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new JavaHelloWorld().getGreeting());
     }
 }
--- sem01/scala/maga/src/main/scala/org/khomenko/maga/HelloWorld.scala
+++ sem01/scala/maga/src/main/scala/org/khomenko/maga/HelloWorld.scala
@@ -1,7 +1,5 @@
 package org.khomenko.maga
 
-object HelloWorld {
-    def main(args: Array[String]): Unit = {
-        println("Hello, world!")
-    }
+object HelloWorld extends App {
+    println("Hello, world!");
 }
