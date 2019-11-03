1768202e23b406eb9fb2cff2cc8b120ae625eceb
Roman Khomenko <romankhomenko1995@gmail.com>
2019-11-03 23:09:53 +0300

    Реализована RBF сеть

--- sem01/data-proc/optimization/rbf.py
+++ sem01/data-proc/optimization/rbf.py
@@ -1,54 +1,78 @@
 import numpy as np
-from scipy.optimize import minimize
-import matplotlib.pyplot as plt
+from scipy.optimize import minimize as sc_minimize
 
 
-def opt_grad(func, x0, alpha, eps):
-    e = 1e-4
-    xk = x0
+class RBFNet:
+    def __init__(self, rbf_num):
+        self.rbf_num = rbf_num
+        self.minimize = lambda f, x0, jac=None: sc_minimize(
+            f, x0, method='Nelder-Mead', options={'maxiter': 20000})
+        self.w = None
+        self.e = None
+        self.b = None
+        self.theta = None
 
-    while True:
-        print(xk)
-        xk1 = xk - alpha * (func(xk + e) - func(xk - e)) / 2 / e
+    def __set_w(self, w):
+        self.w = w
 
-        if np.linalg.norm(xk1 - xk) < eps:
-            break
+    def __set_e(self, e):
+        self.e = e
 
-        xk = xk1
+    def __set_b(self, b):
+        self.b = b
 
-    return xk1
+    def __set_theta(self, theta):
+        self.theta = theta
 
+    def get_rbf_num(self):
+        return self.rbf_num
 
-def f(x):
-    return (x - 1) ** 2 + 1
+    def get_minimize(self):
+        return self.minimize
 
+    def get_theta(self):
+        return self.theta
 
-def h(x, theta, N):
-    m = x.shape[0]
+    @staticmethod
+    def __h_helper(x, w, e, b):
+        return w * np.exp(-e * np.linalg.norm(x.T - b, axis=-1))
 
-    w = theta[:N]
-    e = theta[N:2 * N]
-    b = theta[2 * N:2 * N + N * m].reshape((N, m))
+    @staticmethod
+    def __h(X, theta, rbf_num):
+        m = X.shape[1]
 
-    return w * np.exp(-e * np.linalg.norm(x.T - b, axis=-1))
+        w = theta[:rbf_num]
+        e = theta[rbf_num:2 * rbf_num]
+        b = theta[2 * rbf_num:2 * rbf_num + rbf_num * m].reshape((rbf_num, m))
 
+        h_row_wise = np.sum(np.apply_along_axis(
+            lambda x: RBFNet.__h_helper(x, w, e, b), arr=X, axis=1), axis=1)
 
-def J(x, y, theta, N):
-    return np.sum((h(x, theta, N) - y) ** 2 )
+        return h_row_wise
 
+    @staticmethod
+    def J(X, y, theta, rbf_num):
+        return np.sum((RBFNet.__h(X, theta, rbf_num) - y) ** 2)
 
-n = 5
-m = 3
-N = 2
+    def train(self, X, y, theta0=None):
+        m = X.shape[1]
+        N = self.get_rbf_num()
+        minimize = self.get_minimize()
 
-x = np.ones(m)
-y = np.array([1])
-theta0 = np.random.uniform(0, 1, size=(2 * N + N * m))
+        if not theta0:
+            theta0 = np.random.uniform(0, 1, size=(2 * N + N * m))
 
-j = J(x, y, theta0, N)
-print('Error on random theta:', J(x, y, theta0, N))
+        opt_result = minimize(lambda theta: RBFNet.J(X, y, theta, N), x0=theta0)
 
-opt_result = minimize(lambda theta: J(x, y, theta, N), x0=theta0, method='Nelder-Mead')
-theta_o = opt_result.x
-print('Theta opt:', theta_o)
-print('Error on theta opt:', J(x, y, theta_o, N))
+        if not opt_result.success:
+            raise RuntimeError('Method does not converged')
+
+        theta_opt = opt_result.x
+
+        self.__set_w(theta_opt[:N])
+        self.__set_e(theta_opt[N:2 * N])
+        self.__set_b(theta_opt[2 * N:].reshape((N, m)))
+        self.__set_theta(theta_opt)
+
+    def fit(self, X):
+        return RBFNet.__h(X, self.get_theta(), self.get_rbf_num())
