3743b5f59d22992a45c0122ed5f38d4f2d26325b
Roman Khomenko <romankhomenko1995@gmail.com>
2019-11-14 23:26:33 +0300

    Реализовано вычисление градиентов

--- sem01/data-proc/optimization/rbf.py
+++ sem01/data-proc/optimization/rbf.py
@@ -1,6 +1,7 @@
 import abc
 import numpy as np
 from scipy.optimize import minimize as sc_minimize
+import matplotlib.pyplot as plt
 import torch
 
 
@@ -10,25 +11,27 @@ class RBFNetBase(metaclass=abc.ABCMeta):
         self.theta = None
         self.x_dim = 0
 
-    def _split_theta(self):
-        w = self.theta[:self.n_components]
-        e = self.theta[self.n_components:2 * self.n_components]
-        b = self.theta[2 * self.n_components:2 * self.n_components + self.n_components * self.x_dim] \
+    def split_theta(self, theta):
+        w = theta[:self.n_components]
+        e = theta[self.n_components:2 * self.n_components]
+        b = theta[2 * self.n_components:2 * self.n_components + self.n_components * self.x_dim] \
             .reshape((self.n_components, self.x_dim))
 
         return w, e, b
 
-    def h_split(self, X):
-        w, e, b = self._split_theta()
-        return np.apply_along_axis(lambda x: w * np.exp(-e * np.linalg.norm(x.T - b, axis=-1) ** 2),
-                                   arr=X, axis=1)
+    def _h_helper(self, x, w, e, b):
+        return w * np.exp(-e * (x - b.T) ** 2)
 
-    def h(self, X):
-        h_row_wise = np.sum(self.h_split(X), axis=1)
+    def h_split(self, theta, X):
+        w, e, b = self.split_theta(theta)
+        return np.apply_along_axis(lambda x: self._h_helper(x, w, e, b), arr=X, axis=1)
+
+    def h(self, theta, X):
+        h_row_wise = np.sum(self.h_split(theta, X), axis=-1)
         return h_row_wise.reshape((X.shape[0], X.shape[1]))
 
     def fit(self, X):
-        return self.h(X)
+        return self.h(self.get_theta(), X)
 
     @abc.abstractmethod
     def train(self, X, y, theta0=None, epochs=1000, eps=1e-6):
@@ -47,7 +50,7 @@ class RBFNetBase(metaclass=abc.ABCMeta):
         self.theta = theta
 
     def get_params(self):
-        return self._split_theta()
+        return self.split_theta(self.get_theta())
 
     def get_n_components(self):
         return self.n_components
@@ -103,103 +106,46 @@ class RBFNetTorch(RBFNetBase):
         return loss_arr
 
 
-class RBFNet:
-    def __init__(self, rbf_num):
-        self.rbf_num = rbf_num
-        self.minimize = lambda f, x0, jac=None: sc_minimize(
-            f, x0, method='Nelder-Mead', options={'maxiter': 20000})
-        self.w = None
-        self.e = None
-        self.b = None
-        self.theta = None
+class RBFNetScipy(RBFNetBase):
+    def grad(self, theta, X, y):
+        w, e, b = self.split_theta(theta)
 
-    def __set_w(self, w):
-        self.w = w
+        dw = 2 * np.sum((self.h(theta, X) - y) * np.exp(-e * (X - b.T) ** 2), axis=0) / X.shape[0]
+        de = -2 * np.sum((self.h(theta, X) - y) * w * np.exp(-e * (X - b.T) ** 2) * (X - b.T) ** 2, axis=0) / X.shape[0]
+        db = 4 * np.sum((self.h(theta, X) - y) * w * np.exp(-e * (X - b.T) ** 2) * e.T * (X - b.T), axis=0) / X.shape[0]
+        return np.hstack((dw, de, db))
 
-    def __set_e(self, e):
-        self.e = e
+    def train(self, X, y, theta0=None, epochs=1000, eps=1e-6):
+        self.set_x_dim(X.shape[1])
 
-    def __set_b(self, b):
-        self.b = b
+        theta = np.random.rand(2 * self.get_n_components() + self.get_n_components() * self.get_x_dim()) * 5 \
+            if not theta0 else theta0
 
-    def set_theta(self, theta):
-        self.theta = theta
+        print('theta0:', theta)
+        # tol = 1
+        # iter = 0
+        # while iter < epochs and tol > eps:
+        #     grad = self.grad(theta, X, y)
+        #     theta -= 0.01 * grad
+        #
+        #     if iter % 10 == 0:
+        #         print('Theta:', theta, '|grad|: ', np.linalg.norm(grad))
+        #
+        #     iter += 1
 
-    def get_rbf_num(self):
-        return self.rbf_num
+        x = sc_minimize(lambda theta: np.sum((self.h(theta, X) - y) ** 2 / X.shape[0]),
+                        theta,
+                        jac=lambda theta: self.grad(theta, X, y),
+                        method='CG')
+        print(x)
+        print(np.sum((self.h(x.x, X) - y) ** 2))
 
-    def get_minimize(self):
-        return self.minimize
+        plt.plot(X, y)
+        plt.plot(X, self.h(x.x, X))
+        plt.show()
 
-    def get_theta(self):
-        return self.theta
+X = np.linspace(0, 10, 100).reshape((100, 1))
+y = 10 * np.exp(- 1.5 * (X - 5) ** 2) + 5 * np.exp(-0.5 * (X - 8) ** 2) + 8 * np.sin(X) + 10
 
-    def get_params(self):
-        return self.w, self.e, self.b
-
-    @staticmethod
-    def __split_theta(theta, rbf_num, m):
-        w = theta[:rbf_num]
-        e = theta[rbf_num:2 * rbf_num]
-        b = theta[2 * rbf_num:2 * rbf_num + rbf_num * m].reshape((rbf_num, m))
-
-        return w, e, b
-
-    @staticmethod
-    def __h_helper(x, w, e, b):
-        return w * np.exp(-e * np.linalg.norm(x.T - b, axis=-1) ** 2)
-
-    @staticmethod
-    def h_split(X, theta, rbf_num):
-        w, e, b = RBFNet.__split_theta(theta, rbf_num, X.shape[1])
-        return np.apply_along_axis(lambda x: RBFNet.__h_helper(x, w, e, b), arr=X, axis=1)
-
-    @staticmethod
-    def h(X, theta, rbf_num):
-        h_row_wise = np.sum(RBFNet.h_split(X, theta, rbf_num), axis=1)
-        return h_row_wise.reshape((X.shape[0], X.shape[1]))
-
-    @staticmethod
-    def J(X, y, theta, rbf_num):
-        return np.sum((RBFNet.h(X, theta, rbf_num) - y) ** 2) / 2 / X.shape[0]
-
-    @staticmethod
-    def J_der(X, y, theta, rbf_num):
-        w, e, b = RBFNet.__split_theta(theta, rbf_num, X.shape[1])
-        err = RBFNet.h(X, theta, rbf_num) - y
-
-        theta_w = theta.copy()
-        theta_w[:rbf_num] = np.ones(rbf_num)
-        w_der = err * RBFNet.h_split(X, theta_w, rbf_num)
-
-        print(w_der.shape)
-
-        # e_der = s * np.apply_along_axis(
-        #     lambda x: - np.linalg.norm(x.T - b, axis=-1) ** 2 * w * np.exp(-e * np.linalg.norm(x.T - b, axis=-1) ** 2),
-        #     arr=X, axis=1)
-        # b_der = s * 3
-
-    def train(self, X, y, theta0=None):
-        m = X.shape[1]
-        rbf_num = self.get_rbf_num()
-        minimize = self.get_minimize()
-
-        if not theta0:
-            theta0 = np.random.uniform(-1, 1, size=(2 * rbf_num + rbf_num * m))
-
-        opt_result = minimize(lambda theta: RBFNet.J(X, y, theta, rbf_num), x0=theta0)
-
-        if not opt_result.success:
-            raise RuntimeError('Method does not converged')
-
-        theta_opt = opt_result.x
-
-        w, e, b = RBFNet.__split_theta(X, self.get_rbf_num(), m)
-
-        self.__set_w(w)
-        self.__set_e(e)
-        self.__set_b(b)
-        self.set_theta(theta_opt)
-
-    def fit(self, X):
-        return RBFNet.h(X, self.get_theta(), self.get_rbf_num())
+net = RBFNetScipy(n_components=5)
+net.train(X, y, epochs=20000)
